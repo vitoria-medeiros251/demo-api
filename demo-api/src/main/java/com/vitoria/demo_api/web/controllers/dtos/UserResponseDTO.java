8d57c0fd40355968b14b8a47c61a92b749008960
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2026-03-03 10:20:05 -0300

    feat(user): adicionar UserResponseDTO na consulta por ID

---
+++ demo-api/src/main/java/com/vitoria/demo_api/web/controllers/dtos/UserResponseDTO.java
@@ -0,0 +1,16 @@
+package com.vitoria.demo_api.web.controllers.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDTO {
+
+    private Long id;
+    private String username;
+    private String role;
+
+}
