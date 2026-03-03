bb835dab861703e9230318b41a12f94a6045131e
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2026-03-03 17:35:45 -0300

    git commit -m "validando o objeto  userCreateDto"

--- demo-api/src/main/java/com/vitoria/demo_api/web/controllers/dtos/UserCreateDTO.java
+++ demo-api/src/main/java/com/vitoria/demo_api/web/controllers/dtos/UserCreateDTO.java
@@ -1,5 +1,8 @@
 package com.vitoria.demo_api.web.controllers.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserCreateDTO {
+    @NotBlank
+    @Email(message= "formato do email é invalido ",regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
     private String username;
+    @NotBlank
+    @Size(min = 6,max = 6)
     private String password;
 }
