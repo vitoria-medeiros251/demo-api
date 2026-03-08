222819b26c2d96fc5d622455832bff2bd680714d
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2026-03-08 15:26:37 -0300

    git commit -m "validando o objeto  UserSenhaDTO"

--- demo-api/src/main/java/com/vitoria/demo_api/web/controllers/dtos/UserSenhaDTO.java
+++ demo-api/src/main/java/com/vitoria/demo_api/web/controllers/dtos/UserSenhaDTO.java
@@ -1,6 +1,8 @@
 package com.vitoria.demo_api.web.controllers.dtos;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserSenhaDTO {
+    @NotBlank
+    @Size(min = 6,max = 6)
     private String senhaAtual;
+    @NotBlank
+    @Size(min = 6,max = 6)
     private String novaSenha;
+    @NotBlank
+    @Size(min = 6,max = 6)
     private String confirmaSenha;
 }
 
