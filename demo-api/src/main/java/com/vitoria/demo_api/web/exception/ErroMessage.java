51fbfc9362913e2253b7c6d5f6815efca0c0d316
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2026-03-09 15:37:28 -0300

    git commit -m"Objeto de erro para respostas de requisiçoes"

---
+++ demo-api/src/main/java/com/vitoria/demo_api/web/exception/ErroMessage.java
@@ -0,0 +1,45 @@
+package com.vitoria.demo_api.web.exception;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+
+import java.util.Map;
+
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class ErroMessage {
+
+    private String path;
+    private String method;
+    private int status;
+    private String statusText;
+    private String message;
+    private Map<String,String> errors;
+
+    public static ErroMessage of(HttpServletRequest request, HttpStatus status, String message, BindingResult result){
+        ErroMessage erroMessage = ErroMessage.builder()
+                .path(request.getRequestURI())
+                .method(request.getMethod())
+                .status(status.value())
+                .statusText(status.getReasonPhrase())
+                .message(message)
+                .build();
+        erroMessage.addErrors(result);
+        return erroMessage;
+    }
+
+    private void addErrors(BindingResult result){
+        this.errors = new java.util.HashMap<>();
+        result.getFieldErrors().forEach(error -> 
+            this.errors.put(error.getField(), error.getDefaultMessage())
+        );
+    }
+    
+
+}
