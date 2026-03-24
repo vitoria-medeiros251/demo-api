32daa94a9890d172d5f7aa13f7346aac150a4e67
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2026-03-24 12:08:05 -0300

    git commit -m "configurando ambiente de testes "

---
+++ demo-api/src/test/java/com/vitoria/demo_api/UserIT.java
@@ -0,0 +1,17 @@
+package com.vitoria.demo_api;
+
+import org.hibernate.annotations.processing.SQL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/users/users-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/users/users-insert.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class UserIT {
+    @Autowired
+    WebTestClient testClient;
+
+
+}
---
+++ demo-api/src/test/resources/sql/users/users-insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO USERS(id,username,passoword,role) values(100,ana@email.com,12345,'ROLE_ADMIN');
+INSERT INTO USERS(id,username,passoword,role) values(101,bia@email.com,12345,'ROLE_CLIENT');
+INSERT INTO USERS(id,username,passoword,role) values(102,bob@email.com,12345,'ROLE_CLIENT');
