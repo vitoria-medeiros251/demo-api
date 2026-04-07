eff77f2e4a8000b16dffa20b097248e16bc33745
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2026-04-07 15:46:07 -0300

    git commit -m "Teste de ponta a ponta para localizar usuario por id "

--- demo-api/src/test/java/com/vitoria/demo_api/UserIT.java
+++ demo-api/src/test/java/com/vitoria/demo_api/UserIT.java
@@ -149,4 +149,23 @@ public class UserIT {
         org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
 
     }
+
+    @Test
+    public void BuscarUser_ComIdExistente_RetornarUserComStatus200() {
+
+        UserResponseDTO responseBody = testClient
+                .get()
+                .uri("/api/v1/users/100")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(UserResponseDTO.class)
+                .returnResult().getResponseBody();
+
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isEqualTo(100);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("ana@email.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("ADMIN");
+
+    }
 }
